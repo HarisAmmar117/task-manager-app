148b43deecf71fca62102e9436e83630fc909769
HarisAmmar117 <mhmammar19@gmail.ccom>
2026-03-20 11:10:05 +0530

    Created model task and implemented it

---
+++ backend/src/main/java/com/taskflow/backend/Task.java
@@ -0,0 +1,21 @@
+package com.taskflow.backend;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity(name= "tasks")
+public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String description;
+    private String status;
+    private LocalDateTime createdAt;
+}
