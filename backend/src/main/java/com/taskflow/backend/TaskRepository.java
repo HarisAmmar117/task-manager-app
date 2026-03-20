f7c713fbf4893042151520727592ff094d4f4329
HarisAmmar117 <mhmammar19@gmail.ccom>
2026-03-20 11:25:58 +0530

    Created controller, service, repository files

---
+++ backend/src/main/java/com/taskflow/backend/TaskRepository.java
@@ -0,0 +1,9 @@
+package com.taskflow.backend;
+
+import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+@Repository
+public interface TaskRepository extends JpaRepository<Task, Long> {
+
+}
