d291735eb1970060b46e8d41ccfcbaf4984d7f44
HarisAmmar117 <mhmammar19@gmail.ccom>
2026-03-20 14:52:08 +0530

    configured mysql to the project and fixed error in adding task function

--- backend/src/main/java/com/taskflow/backend/TaskController.java
+++ backend/src/main/java/com/taskflow/backend/TaskController.java
@@ -2,6 +2,7 @@ package com.taskflow.backend;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 @RequestMapping("/api/tasks")
 public class TaskController {
 
+    @Autowired
     TaskService taskService;
 
 
