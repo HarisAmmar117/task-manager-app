31fea3a7df68423e02c6b0fc8b6837afefc6c121
HarisAmmar117 <mhmammar19@gmail.ccom>
2026-03-20 14:06:30 +0530

    created functions in controler,service to create task and also created methods to map request to task

--- backend/src/main/java/com/taskflow/backend/TaskController.java
+++ backend/src/main/java/com/taskflow/backend/TaskController.java
@@ -2,10 +2,34 @@ package com.taskflow.backend;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 @RestController
 @RequestMapping("/api/tasks")
 public class TaskController {
+
+    TaskService taskService;
+
+
+    @PostMapping
+    public ResponseEntity<String> addTask(@RequestBody TaskRequest taskRequest) {
+        
+        boolean isSuccessful = taskService.createTask(taskRequest);
+        
+        if (isSuccessful) {
+            return ResponseEntity.status(HttpStatus.CREATED)
+                            .body("Task Successfully Created");
+        }
+        
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                        .body("Task Creation Failed");
+    }
+   
+    
 
     
 
--- backend/src/main/java/com/taskflow/backend/TaskService.java
+++ backend/src/main/java/com/taskflow/backend/TaskService.java
@@ -2,7 +2,33 @@ package com.taskflow.backend;
 
 import org.springframework.stereotype.Service;
 
+import lombok.RequiredArgsConstructor;
+
 @Service
+@RequiredArgsConstructor
 public class TaskService {
+
+    private final TaskRepository taskRepository;
+
+    public boolean createTask(TaskRequest taskRequest) {
+        try {
+            Task task = new Task();
+            updateTaskFromRequest(task, taskRequest);
+            taskRepository.save(task);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+
+    public void updateTaskFromRequest(Task task, TaskRequest request){
+
+        task.setTitle(request.getTitle());
+        task.setDescription(request.getDescription());
+        task.setStatus(request.getStatus());
+
+
+    }
     
 }
