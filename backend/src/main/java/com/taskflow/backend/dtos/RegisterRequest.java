7ffbe9cefcf7429cfe70b2c8d9735767da428341
HarisAmmar117 <mhmammar19@gmail.ccom>
2026-03-23 17:48:05 +0530

    added dependencies and created register request

---
+++ backend/src/main/java/com/taskflow/backend/dtos/RegisterRequest.java
@@ -0,0 +1,10 @@
+package com.taskflow.backend.dtos;
+
+public class RegisterRequest {
+
+
+    private String username;
+    private String password;
+    private String email;
+    private String fullName;
+}
---
+++ backend/src/main/java/com/taskflow/backend/services/UserService.java
@@ -0,0 +1,13 @@
+package com.taskflow.backend.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.taskflow.backend.models.User;
+import com.taskflow.backend.repositories.UserRepository;
+
+@Service
+public class UserService {
+    
+}
+
